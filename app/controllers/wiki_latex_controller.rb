81bfeb70071a75df06fcbd64a84df2f371b2449c
megazet404 <57397480+megazet404@users.noreply.github.com>
2021-11-19 22:27:36 +1000

    Create the LatexProcessor class. LatexProcessor is a class that processes LaTeX source code and generates image from it. WikiLatexController is now responsible only for handling HTTP requests and sending image files. Image file creation is now delegated to LatexProcessor.

--- app/controllers/wiki_latex_controller.rb
+++ app/controllers/wiki_latex_controller.rb
@@ -1,9 +1,49 @@
 class WikiLatexController < ApplicationController
+  class LatexProcessor
+    def self.make_png(name)
+      LatexProcessor.new(name).make_png()
+    end
 
-  PATH = (WikiLatexConfig::TOOLS_PATH == "" ? "" : File.join(WikiLatexConfig::TOOLS_PATH, ""))
+    PATH = (WikiLatexConfig::TOOLS_PATH == "" ? "" : File.join(WikiLatexConfig::TOOLS_PATH, ""))
+
+    def initialize(name)
+      @name  = name
+      @latex = WikiLatex.find_by_image_id(@name)
+    end
+
+    def make_png()
+      dir = WikiLatexHelper::DIR
+      begin
+        Dir.mkdir(dir)
+      rescue
+      end
+      basefilename = File.join([dir,@name])
+      temp_latex = File.open(basefilename+".tex",'wb')
+      temp_latex.print('\input{../../plugins/wiki_latex/assets/latex/header.tex}', "\n")
+      temp_latex.print(@latex.preamble.gsub('\\\\','\\').gsub(/\r\n?/, "\n"), "\n")
+      temp_latex.print('\input{../../plugins/wiki_latex/assets/latex/header2.tex}', "\n")
+      temp_latex.print(@latex.source.gsub('\\\\','\\').gsub(/\r\n?/, "\n"), "\n")
+      temp_latex.print('\input{../../plugins/wiki_latex/assets/latex/footer.tex}', "\n")
+      temp_latex.flush
+      temp_latex.close
+
+      if WikiLatexConfig::Png::GRAPHICS_SUPPORT
+        system("cd #{dir} && #{PATH}pdflatex --interaction=nonstopmode #{@name}.tex")
+        system("cd #{dir} && #{PATH}pdftops -eps #{@name}.pdf")
+        system("cd #{dir} && #{PATH}convert -density 100 #{@name}.eps #{@name}.png")
+      else
+        system("cd #{dir} && #{PATH}latex --interaction=nonstopmode #{@name}.tex")
+        system("cd #{dir} && #{PATH}dvipng -T tight -bg Transparent #{@name}.dvi -o #{@name}.png")
+      end
+      ['tex','pdf','eps','dvi', 'log','aux'].each do |ext|
+        if File.exists?(basefilename+"."+ext)
+          File.unlink(basefilename+"."+ext)
+        end
+      end
+    end
+  end
 
   def image
-    @latex = WikiLatex.find_by_image_id(params[:image_id])
     @name = params[:image_id]
     if @name != "error"
 	image_file = File.join([WikiLatexHelper::DIR, @name+".png"])
@@ -12,7 +52,7 @@ class WikiLatexController < ApplicationController
     end
 
     if (!File.exists?(image_file))
-    	render_image
+    	LatexProcessor.make_png(@name)
     end
     if (File.exists?(image_file))
       #render :file => image_file, :layout => false, :content_type => 'image/png'
@@ -25,38 +65,4 @@ class WikiLatexController < ApplicationController
     rescue ActiveRecord::RecordNotFound
       render_404
   end
-
-private
-  def render_image
-    dir = WikiLatexHelper::DIR
-    begin
-      Dir.mkdir(dir)
-    rescue
-    end
-    basefilename = File.join([dir,@name])
-    temp_latex = File.open(basefilename+".tex",'wb')
-    temp_latex.print('\input{../../plugins/wiki_latex/assets/latex/header.tex}', "\n")
-    temp_latex.print(@latex.preamble.gsub('\\\\','\\').gsub(/\r\n?/, "\n"), "\n")
-    temp_latex.print('\input{../../plugins/wiki_latex/assets/latex/header2.tex}', "\n")
-    temp_latex.print(@latex.source.gsub('\\\\','\\').gsub(/\r\n?/, "\n"), "\n")
-    temp_latex.print('\input{../../plugins/wiki_latex/assets/latex/footer.tex}', "\n")
-    temp_latex.flush
-    temp_latex.close
-
-    if WikiLatexConfig::Png::GRAPHICS_SUPPORT
-      system("cd #{dir} && #{PATH}pdflatex --interaction=nonstopmode #{@name}.tex")
-      system("cd #{dir} && #{PATH}pdftops -eps #{@name}.pdf")
-      system("cd #{dir} && #{PATH}convert -density 100 #{@name}.eps #{@name}.png")
-    else
-      system("cd #{dir} && #{PATH}latex --interaction=nonstopmode #{@name}.tex")
-      system("cd #{dir} && #{PATH}dvipng -T tight -bg Transparent #{@name}.dvi -o #{@name}.png")
-    end
-    ['tex','pdf','eps','dvi', 'log','aux'].each do |ext|
-    if File.exists?(basefilename+"."+ext)
-        File.unlink(basefilename+"."+ext)
-	end
-    end
-  end
-
 end
-
