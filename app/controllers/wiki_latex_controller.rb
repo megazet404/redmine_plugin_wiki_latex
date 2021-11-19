5f09847e031a2fea437006016adeb0d8e2b92f1d
megazet404 <57397480+megazet404@users.noreply.github.com>
2021-11-19 23:48:47 +1000

    Do not hardcode directory in LatexProcessor. Make LatexProcessor able to process LaTeX sources in any directory. Full path to .tex file is now passed to LatexProcessor.

--- app/controllers/wiki_latex_controller.rb
+++ app/controllers/wiki_latex_controller.rb
@@ -1,24 +1,25 @@
 class WikiLatexController < ApplicationController
   class LatexProcessor
-    def self.make_png(name)
-      LatexProcessor.new(name).make_png()
+    def self.make_png(basefilename)
+      LatexProcessor.new(basefilename).make_png()
     end
 
     PATH = (WikiLatexConfig::TOOLS_PATH == "" ? "" : File.join(WikiLatexConfig::TOOLS_PATH, ""))
 
-    def initialize(name)
-      @name  = name
+    def initialize(basefilename)
+      @basefilename = basefilename
+      @dir          = File.dirname (@basefilename)
+      @name         = File.basename(@basefilename)
+
       @latex = WikiLatex.find_by_image_id(@name)
     end
 
     def make_png()
-      dir = WikiLatexHelper::DIR
       begin
-        Dir.mkdir(dir)
+        Dir.mkdir(@dir)
       rescue
       end
-      basefilename = File.join([dir,@name])
-      temp_latex = File.open(basefilename+".tex",'wb')
+      temp_latex = File.open(@basefilename+".tex",'wb')
       temp_latex.print('\input{../../plugins/wiki_latex/assets/latex/header.tex}', "\n")
       temp_latex.print(@latex.preamble.gsub('\\\\','\\').gsub(/\r\n?/, "\n"), "\n")
       temp_latex.print('\input{../../plugins/wiki_latex/assets/latex/header2.tex}', "\n")
@@ -28,31 +29,32 @@ class WikiLatexController < ApplicationController
       temp_latex.close
 
       if WikiLatexConfig::Png::GRAPHICS_SUPPORT
-        system("cd #{dir} && #{PATH}pdflatex --interaction=nonstopmode #{@name}.tex")
-        system("cd #{dir} && #{PATH}pdftops -eps #{@name}.pdf")
-        system("cd #{dir} && #{PATH}convert -density 100 #{@name}.eps #{@name}.png")
+        system("cd #{@dir} && #{PATH}pdflatex --interaction=nonstopmode #{@name}.tex")
+        system("cd #{@dir} && #{PATH}pdftops -eps #{@name}.pdf")
+        system("cd #{@dir} && #{PATH}convert -density 100 #{@name}.eps #{@name}.png")
       else
-        system("cd #{dir} && #{PATH}latex --interaction=nonstopmode #{@name}.tex")
-        system("cd #{dir} && #{PATH}dvipng -T tight -bg Transparent #{@name}.dvi -o #{@name}.png")
+        system("cd #{@dir} && #{PATH}latex --interaction=nonstopmode #{@name}.tex")
+        system("cd #{@dir} && #{PATH}dvipng -T tight -bg Transparent #{@name}.dvi -o #{@name}.png")
       end
       ['tex','pdf','eps','dvi', 'log','aux'].each do |ext|
-        if File.exists?(basefilename+"."+ext)
-          File.unlink(basefilename+"."+ext)
+        if File.exists?(@basefilename+"."+ext)
+          File.unlink(@basefilename+"."+ext)
         end
       end
     end
   end
 
   def image
-    @name = params[:image_id]
-    if @name != "error"
-	image_file = File.join([WikiLatexHelper::DIR, @name+".png"])
+    name = params[:image_id]
+    basefilename = File.join(WikiLatexHelper::DIR, name)
+    if name != "error"
+	image_file = "#{basefilename}.png"
     else
-	image_file = File.join([Rails.root, 'public', 'plugin_assets', 'wiki_latex', 'images', @name+".png"])
+	image_file = File.join([Rails.root, 'public', 'plugin_assets', 'wiki_latex', 'images', name+".png"])
     end
 
     if (!File.exists?(image_file))
-    	LatexProcessor.make_png(@name)
+    	LatexProcessor.make_png(basefilename)
     end
     if (File.exists?(image_file))
       #render :file => image_file, :layout => false, :content_type => 'image/png'
