2a1e369556bb39bffba1e9baf502e9783772031c
megazet404 <57397480+megazet404@users.noreply.github.com>
2021-11-20 00:26:34 +1000

    Rename 'basefilename' to 'basefilepath'.

--- app/controllers/wiki_latex_controller.rb
+++ app/controllers/wiki_latex_controller.rb
@@ -1,15 +1,15 @@
 class WikiLatexController < ApplicationController
   class LatexProcessor
-    def self.make_png(basefilename)
-      LatexProcessor.new(basefilename).make_png()
+    def self.make_png(basefilepath)
+      LatexProcessor.new(basefilepath).make_png()
     end
 
     PATH = (WikiLatexConfig::TOOLS_PATH == "" ? "" : File.join(WikiLatexConfig::TOOLS_PATH, ""))
 
-    def initialize(basefilename)
-      @basefilename = basefilename
-      @dir          = File.dirname (@basefilename)
-      @name         = File.basename(@basefilename)
+    def initialize(basefilepath)
+      @basefilepath = basefilepath
+      @dir          = File.dirname (@basefilepath)
+      @name         = File.basename(@basefilepath)
 
       @latex = WikiLatex.find_by_image_id(@name)
     end
@@ -19,7 +19,7 @@ class WikiLatexController < ApplicationController
         Dir.mkdir(@dir)
       rescue
       end
-      temp_latex = File.open(@basefilename+".tex",'wb')
+      temp_latex = File.open(@basefilepath+".tex",'wb')
       temp_latex.print('\input{../../plugins/wiki_latex/assets/latex/header.tex}', "\n")
       temp_latex.print(@latex.preamble.gsub('\\\\','\\').gsub(/\r\n?/, "\n"), "\n")
       temp_latex.print('\input{../../plugins/wiki_latex/assets/latex/header2.tex}', "\n")
@@ -37,8 +37,8 @@ class WikiLatexController < ApplicationController
         system("cd #{@dir} && #{PATH}dvipng -T tight -bg Transparent #{@name}.dvi -o #{@name}.png")
       end
       ['tex','pdf','eps','dvi', 'log','aux'].each do |ext|
-        if File.exists?(@basefilename+"."+ext)
-          File.unlink(@basefilename+"."+ext)
+        if File.exists?(@basefilepath+"."+ext)
+          File.unlink(@basefilepath+"."+ext)
         end
       end
     end
@@ -46,15 +46,15 @@ class WikiLatexController < ApplicationController
 
   def image
     name = params[:image_id]
-    basefilename = File.join(WikiLatexHelper::DIR, name)
+    basefilepath = File.join(WikiLatexHelper::DIR, name)
     if name != "error"
-	image_file = "#{basefilename}.png"
+	image_file = "#{basefilepath}.png"
     else
 	image_file = File.join([Rails.root, 'public', 'plugin_assets', 'wiki_latex', 'images', name+".png"])
     end
 
     if (!File.exists?(image_file))
-    	LatexProcessor.make_png(basefilename)
+    	LatexProcessor.make_png(basefilepath)
     end
     if (File.exists?(image_file))
       #render :file => image_file, :layout => false, :content_type => 'image/png'
