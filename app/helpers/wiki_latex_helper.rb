a9a074f5d0d90c1f8b6f13c405d4cba305013b15
megazet404 <57397480+megazet404@users.noreply.github.com>
2021-11-20 00:48:39 +1000

    Fix indentation in wiki_latex_helper.rb.

--- app/helpers/wiki_latex_helper.rb
+++ app/helpers/wiki_latex_helper.rb
@@ -1,5 +1,5 @@
 require 'digest/sha2'
-require	'tempfile'
+require 'tempfile'
 
 module WikiLatexHelper
 
@@ -16,47 +16,47 @@ module WikiLatexHelper
   def render_image_block(image_name, preamble, source, wiki_name)
     render_to_string :template => 'wiki_latex/macro_block', :layout => false, :locals => {:name => image_name, :source => source, :preamble => preamble, :wiki_name => wiki_name}
   end
-	class Macro
-		def initialize(view, source)
-		  @view = view
-		  @view.controller.extend(WikiLatexHelper)
-		        preamble = ' '
- 			unless source.nil? || source.empty?
-                          if source.include? '|||||'
-			    ary = source.split('|||||')
-			    source = ary[1]
-			    preamble = ary[0]
-                          end
-			  source.gsub!(/<br \/>/,"")
-			  source.gsub!(/<\/?p>/,"")
-			  source.gsub!(/<\/?div>/,"")
- 			end
-			unless preamble.nil? || preamble.empty?
-			  preamble.gsub!(/<br \/>/,"")
-			  preamble.gsub!(/<\/?p>/,"")
-			  preamble.gsub!(/<\/?div>/,"")
-    			end
-			name = Digest::SHA256.hexdigest(preamble+source)
-			if !WikiLatex.find_by_image_id(name)
-				@latex = WikiLatex.new(:source => source, :image_id => name, :preamble => preamble)
-				@latex.save
-			end
-			@latex = WikiLatex.find_by_image_id(name)
-		end
+  class Macro
+    def initialize(view, source)
+      @view = view
+      @view.controller.extend(WikiLatexHelper)
+      preamble = ' '
+      unless source.nil? || source.empty?
+        if source.include? '|||||'
+          ary = source.split('|||||')
+          source = ary[1]
+          preamble = ary[0]
+        end
+        source.gsub!(/<br \/>/,"")
+        source.gsub!(/<\/?p>/,"")
+        source.gsub!(/<\/?div>/,"")
+      end
+      unless preamble.nil? || preamble.empty?
+        preamble.gsub!(/<br \/>/,"")
+        preamble.gsub!(/<\/?p>/,"")
+        preamble.gsub!(/<\/?div>/,"")
+      end
+      name = Digest::SHA256.hexdigest(preamble+source)
+      if !WikiLatex.find_by_image_id(name)
+        @latex = WikiLatex.new(:source => source, :image_id => name, :preamble => preamble)
+        @latex.save
+      end
+      @latex = WikiLatex.find_by_image_id(name)
+    end
 
-		def render()
-		  if @latex
-		    @view.controller.render_image_tag(@latex.image_id, @latex.preamble, @latex.source).html_safe
-		  else
-		    @view.controller.render_image_tag("error", "error")
-		  end
-		end
-		def render_block(wiki_name)
-		  if @latex
-		    @view.controller.render_image_block(@latex.image_id, @latex.preamble, @latex.source, wiki_name).html_safe
-		  else
-		    @view.controller.render_image_block("error", "error", wiki_name)
-		  end
-		end
-	end
+    def render()
+      if @latex
+        @view.controller.render_image_tag(@latex.image_id, @latex.preamble, @latex.source).html_safe
+      else
+        @view.controller.render_image_tag("error", "error")
+      end
+    end
+    def render_block(wiki_name)
+      if @latex
+        @view.controller.render_image_block(@latex.image_id, @latex.preamble, @latex.source, wiki_name).html_safe
+      else
+        @view.controller.render_image_block("error", "error", wiki_name)
+      end
+    end
+  end
 end
